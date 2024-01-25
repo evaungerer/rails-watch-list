f58d41325f375b0b2910ee210b3d7f2ccc61e03b
Eva Ungerer <ungerer.eva@gmail.com>
2024-01-25 16:54:25 +0100

    saving current status of watch list challenge

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,18 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    @bookmark.save
+    redirect_to @bookmark.list
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
